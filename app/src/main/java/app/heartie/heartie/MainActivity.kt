4003d26ad38f8a0e2372fe469fb94f00188bf068
Owen Selles <owenselles@protonmail.com>
2019-06-13 22:24:38 +0200

    Add test firebase login

---
+++ app/src/main/java/app/heartie/heartie/MainActivity.kt
@@ -0,0 +1,22 @@
+package app.heartie.heartie
+
+import android.content.Intent
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity;
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        fab.setOnClickListener { view ->
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+}
